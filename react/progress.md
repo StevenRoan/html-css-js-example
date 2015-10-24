006ee314cf6b2637d9aa68861d44cc62c5ffe3a4
ZealotSteven <stevenfriend2001@gmail.com>
2015-10-25 01:05:22 +0800

    learning: basic react with ajax response

---
+++ react/progress.md
@@ -0,0 +1,10 @@
+# Progress
+* Working on tutorial by my self
+
+
+# Todo
+* Append file in fs possible?
+* webpack + babel
+
+# Detected Problem
+* express server terminated will cause an console error in the ajax request. This might implies that the connection is not killed.(POST or GET?)
--- react/server.js
+++ react/server.js
@@ -1,8 +1,17 @@
+var fs = require('fs');
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 app.use(express.static('./'));
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 app.post('/public/data.json', function(req, res, next){
-
+  var path = __dirname+'/public/data.json';
+  var data = JSON.parse(fs.readFileSync(path, "utf8"));
+  data.push(req.body);
+  fs.writeFileSync(path, JSON.stringify(data));
 });
-app.listen(3000);
+var port = 3000;
+app.listen(port, function (){
+  console.log("server listen in port %s", port);
+});
